shashikantkaushik/Home_Service_app

This is the test 3 project in which we have enable login register with otp, firebase integration, and a beautiful ui with category screen, user profile view.

lib/req_model.dart
@@ -1,0 +1,46 @@
+class ReqModel {
+  String name;
+  String bio;
+  String reqPic;
+  String createdAt;
+  String phoneNumber;
+  String uid;
+
+  ReqModel({
+    required this.name,
+    required this.bio,
+    required this.reqPic,
+    required this.createdAt,
+    required this.phoneNumber,
+    required this.uid,
+  });
+
+  // from map
+  factory ReqModel.fromMap(Map<String, dynamic> map) {
+    print("as1df");
+    return ReqModel(
+
+
+      name: map['name'] ?? '',
+      bio: map['bio'] ?? '',
+      uid: map['uid'] ?? '',
+      phoneNumber: map['phoneNumber'] ?? '',
+      createdAt: map['createdAt'] ?? '',
+      reqPic: map['reqPic'] ?? '',
+    );
+  }
+
+  // to map
+  Map<String, dynamic> toMap() {
+    print("a2sdf");
+    return {
+
+      "name": name,
+      "uid": uid,
+      "bio": bio,
+      "reqPic": reqPic,
+      "phoneNumber": phoneNumber,
+      "createdAt": createdAt,
+    };
+  }
+}

shashikantkaushik/homzy2.5

lib/req_model.dart
@@ -1,0 +1,46 @@
+class ReqModel {
+  String name;
+  String about;
+  String reqPic;
+  String createdAt;
+  String phoneNumber;
+  String uid;
+
+  ReqModel({
+    required this.name,
+    required this.about,
+    required this.reqPic,
+    required this.createdAt,
+    required this.phoneNumber,
+    required this.uid,
+  });
+
+  // from map
+  factory ReqModel.fromMap(Map<String, dynamic> map) {
+    print("as1df");
+    return ReqModel(
+
+
+      name: map['name'] ?? '',
+      about: map['about'] ?? '',
+      uid: map['uid'] ?? '',
+      phoneNumber: map['phoneNumber'] ?? '',
+      createdAt: map['createdAt'] ?? '',
+      reqPic: map['reqPic'] ?? '',
+    );
+  }
+
+  // to map
+  Map<String, dynamic> toMap() {
+    print("a2sdf");
+    return {
+
+      "name": name,
+      "uid": uid,
+      "about": about,
+      "reqPic": reqPic,
+      "phoneNumber": phoneNumber,
+      "createdAt": createdAt,
+    };
+  }
+}